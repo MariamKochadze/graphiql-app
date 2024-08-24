98dc3afd71b893e84a814ffb9c5dbf1c835fa153
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-08-24 21:03:41 +0300

    feat: encode and decode response

--- components/RestFull-client/RestfullRoute/Base64Route.tsx
+++ components/RestFull-client/RestfullRoute/Base64Route.tsx
@@ -2,7 +2,7 @@ import { ResponseState } from '@app/common/interface/interface';
 
 export function base64Route(data: ResponseState): string {
   const { url, body, method, headers } = data;
-  const encodedUrl = encodeURIComponent(url);
+  const encodedUrl = encodeURIComponent(btoa(url));
   const encodedBody = body ? btoa(JSON.stringify(body)) : '';
 
   let routeUrl = `/${method}/${encodedUrl}`;
@@ -17,7 +17,7 @@ export function base64Route(data: ResponseState): string {
 }
 export function decodeBase64({ params }: { params: { method: string; base64: string; body?: string } }) {
   const { base64, body } = params;
-  const decodedUrl = decodeURIComponent(base64);
+  const decodedUrl = atob(decodeURIComponent(base64));
   const decodedBody = body ? JSON.parse(atob(body)) : undefined;
 
   return { url: decodedUrl, body: decodedBody };
